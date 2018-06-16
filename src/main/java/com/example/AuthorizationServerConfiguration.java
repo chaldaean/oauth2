a68555977c211e29fb1858c1e02543841b451b35
Vladyslav Marchenko <chaldaean@gmail.com>
2018-06-16 18:33:01 +0300

    refactoring

--- src/main/java/com/example/AuthorizationServerConfiguration.java
+++ src/main/java/com/example/AuthorizationServerConfiguration.java
@@ -1,13 +1,11 @@
 package com.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.annotation.Order;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
@@ -15,7 +13,6 @@ import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
@@ -25,18 +22,12 @@ import javax.servlet.Filter;
 import java.util.ArrayList;
 import java.util.List;
 
-@SpringBootApplication
-@EnableOAuth2Client
+@Configuration
 @EnableAuthorizationServer
-@Order(200)
-public class SocialApplication extends WebSecurityConfigurerAdapter {
+public class AuthorizationServerConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     OAuth2ClientContext oauth2ClientContext;
-
-    public static void main(String[] args) {
-        SpringApplication.run(SocialApplication.class, args);
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -50,7 +41,7 @@ public class SocialApplication extends WebSecurityConfigurerAdapter {
 
     @Bean
     public FilterRegistrationBean<OAuth2ClientContextFilter> oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
-        FilterRegistrationBean<OAuth2ClientContextFilter> registration = new FilterRegistrationBean<OAuth2ClientContextFilter>();
+        FilterRegistrationBean<OAuth2ClientContextFilter> registration = new FilterRegistrationBean<>();
         registration.setFilter(filter);
         registration.setOrder(-100);
         return registration;
